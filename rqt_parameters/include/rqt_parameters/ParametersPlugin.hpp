9863ed00987cadd0fadd6fd53309404b33fd7820
gehrinch <gehrinch@ethz.ch>
2016-08-02 20:16:51 +0200

    Added scrollbar and filter to rqt_parameters

--- rqt_parameters/include/rqt_parameters/ParametersPlugin.hpp
+++ rqt_parameters/include/rqt_parameters/ParametersPlugin.hpp
@@ -10,6 +10,8 @@
 #include <rqt_gui_cpp/plugin.h>
 #include <ui_parameters_plugin.h>
 #include <QWidget>
+#include <QDoubleSpinBox>
+#include <QScrollArea>
 
 #include <ros/ros.h>
 #include <std_srvs/Empty.h>
@@ -18,92 +20,10 @@
 #include <parameter_handler_msgs/SetParameter.h>
 #include <parameter_handler_msgs/GetParameterList.h>
 
-#include <QDoubleSpinBox>
+#include <rqt_parameters/DoubleParameter.hpp>
 
 #include <list>
 #include <memory>
-
-class DoubleParameter: public QObject {
-  Q_OBJECT
- public:
-  DoubleParameter(const std::string& name,
-                  QWidget* widget,
-                  QGridLayout* grid,
-                  ros::ServiceClient* getParameterClient,
-                  ros::ServiceClient* setParameterClient) {
-    name_ = name;
-    grid_ = grid;
-    getParameterClient_ = getParameterClient;
-    setParameterClient_ = setParameterClient;
-
-    std::string lineEditName = std::string{"lineEdit"} + name;
-    lineEditParamName = new QLineEdit(widget);
-    lineEditParamName->setObjectName(QString::fromStdString(lineEditName));
-    lineEditParamName->setMinimumSize(QSize(100, 0));
-    lineEditParamName->setText(QString::fromStdString(name));
-
-    std::string spinBoxName = std::string{"spinBox"} + name;
-    spinBoxParamValue = new QDoubleSpinBox(widget);
-    spinBoxParamValue->setObjectName(QString::fromStdString(spinBoxName));
-    spinBoxParamValue->setMinimumSize(QSize(200, 0));
-    spinBoxParamValue->setAlignment(Qt::AlignRight|Qt::AlignTrailing|Qt::AlignVCenter);
-    spinBoxParamValue->setMaximum(1e+08);
-    spinBoxParamValue->setSingleStep(10);
-
-    std::string pushButtonName = std::string{"pushButtonChange"} + name;
-    pushButtonChangeParam = new QPushButton(widget);
-    pushButtonChangeParam->setObjectName(QString::fromStdString(pushButtonName));
-    pushButtonChangeParam->setMaximumSize(QSize(60, 16777215));
-    pushButtonChangeParam->setText(QApplication::translate("ParametersHandler", "change", 0, QApplication::UnicodeUTF8));
-
-    int iRow = grid->rowCount()+1;
-    grid->addWidget(lineEditParamName,     iRow, 0, 1, 1);
-    grid->addWidget(spinBoxParamValue,     iRow, 1, 1, 1);
-    grid->addWidget(pushButtonChangeParam, iRow, 2, 1, 1);
-
-//    connect(pushButtonChangeParam, SIGNAL(pressed()), this, SLOT(pushButtonChangeParamPressed()));
-    connect(pushButtonChangeParam, SIGNAL(pressed()), this, SLOT(pushButtonChangeParamPressed()));
-
-  }
-  virtual ~DoubleParameter() {
-//    grid_->removeWidget(this);
-    delete lineEditParamName;
-    delete spinBoxParamValue;
-    delete pushButtonChangeParam;
-  };
-
-  public slots:
-
-  void pushButtonChangeParamPressed() {
-    ROS_INFO_STREAM("Change parameter " << name_ << " with value " << spinBoxParamValue->value());
-
-    parameter_handler_msgs::GetParameterRequest reqGet;
-    parameter_handler_msgs::GetParameterResponse resGet;
-
-    reqGet.name = name_;
-    getParameterClient_->call(reqGet, resGet);
-
-    parameter_handler_msgs::SetParameterRequest req;
-    parameter_handler_msgs::SetParameterResponse res;
-
-    req.name = name_;
-    req.value = spinBoxParamValue->value();
-
-    if(!setParameterClient_->call(req, res)) {
-      ROS_ERROR_STREAM("Could not set parameter " << name_);
-    }
-
-  }
- protected:
-  std::string name_;
-  ros::ServiceClient* getParameterClient_;
-  ros::ServiceClient* setParameterClient_;
-  QGridLayout* grid_;
- public:
-  QLineEdit* lineEditParamName;
-  QDoubleSpinBox* spinBoxParamValue;
-  QPushButton* pushButtonChangeParam;
-};
 
 class ParametersPlugin : public rqt_gui_cpp::Plugin {
   Q_OBJECT
@@ -123,6 +43,12 @@ protected:
   private:
   Ui::ParametersHandler ui_;
   QWidget* widget_;
+  QGridLayout* newGridParams_;
+//  QWidget *client;
+//  QScrollArea *area;
+  QWidget* wScroll_;
+  QWidget* client_;
+  QVBoxLayout* layout_;
 
   // Parameters
   ros::ServiceClient getParameterListClient_;
@@ -131,15 +57,17 @@ protected:
 
 
   std::list<std::shared_ptr<DoubleParameter>> doubleParams_;
+  std::vector<std::string> parameters_;
 
  protected slots:
 
   // Parameters
   void pushButtonRefreshAllPressed();
   void pushButtonChangeAllPressed();
+  void drawParamList();
 
 signals:
-    void stateChanged();
+  void parametersChanged();
 
 
 };
--- rqt_parameters/src/ParametersPlugin.cpp
+++ rqt_parameters/src/ParametersPlugin.cpp
@@ -9,6 +9,8 @@
 #include <pluginlib/class_list_macros.h>
 #include <QStringList>
 #include <QGridLayout>
+#include <QScrollArea>
+#include <QScrollBar>
 #include <rqt_parameters/ParametersPlugin.hpp>
 #include <ros/package.h>
 
@@ -16,14 +18,6 @@
 #include <parameter_handler_msgs/SetParameter.h>
 #include <parameter_handler_msgs/GetParameterList.h>
 
-
-#define SPEED_MIN 0.1
-#define SPEED_MAX 4.0
-#define SPEED_RESET 1.0
-
-#define TIME_MIN 0
-#define TIME_MAX 1
-#define TIME_RESET 0.0
 
 template<typename T>
 T mapInRange (T x, T in_min, T in_max, T out_min, T out_max) {
@@ -40,9 +34,29 @@ bool compareNoCase( const std::string& s1, const std::string& s2 ) {
     return strcasecmp( s1.c_str(), s2.c_str() ) <= 0;
 }
 
+struct size_less
+{
+    template<class T> bool operator()(T const &a, T const &b) const
+    { return a.size() < b.size(); }
+};
+
+static size_t max_line_length(std::vector<std::string> const &lines)
+{
+  //use QFontMetrics this way;
+  QFont font("", 0);
+  QFontMetrics fm(font);
+
+  auto it = std::max_element(lines.begin(), lines.end(), size_less());
+  QString text = QString::fromStdString(*it);
+  return fm.width(text);;
+}
+
 ParametersPlugin::ParametersPlugin() :
     rqt_gui_cpp::Plugin(),
-    widget_(0)
+    widget_(0),
+    wScroll_(0),
+    client_(0),
+    layout_(0)
 {
   // Constructor is called first before initPlugin function, needless to say.
   // give QObjects reasonable names
@@ -59,29 +73,19 @@ void ParametersPlugin::initPlugin(qt_gui_cpp::PluginContext& context) {
     ui_.setupUi(widget_);
     context.addWidget(widget_);
 
-
     /******************************
      * Connect ui forms to actions *
      ******************************/
     connect(ui_.pushButtonRefreshAll, SIGNAL(pressed()), this, SLOT(pushButtonRefreshAllPressed()));
     connect(ui_.pushButtonChangeAll, SIGNAL(pressed()), this, SLOT(pushButtonChangeAllPressed()));
-
+    connect(this, SIGNAL(parametersChanged()), this, SLOT(drawParamList()));
     /******************************/
+
 
 
     getParameterClient_ = getNodeHandle().serviceClient<parameter_handler_msgs::GetParameter>("/locomotion_controller/get_parameter");
     setParameterClient_ = getNodeHandle().serviceClient<parameter_handler_msgs::SetParameter>("/locomotion_controller/set_parameter");
     getParameterListClient_ = getNodeHandle().serviceClient<parameter_handler_msgs::GetParameterList>("/locomotion_controller/get_parameter_list");
-
-
-
-    doubleParams_.push_back(std::shared_ptr<DoubleParameter>(new DoubleParameter("test", widget_, ui_.gridParams, &getParameterClient_, &setParameterClient_)));
-    doubleParams_.push_back(std::shared_ptr<DoubleParameter>(new DoubleParameter("test2", widget_, ui_.gridParams, &getParameterClient_, &setParameterClient_)));
-//    int iRow = 3;
-//    ui_.gridParams->addWidget(doubleParams_.back()->lineEditParamName,     iRow, 0, 1, 1);
-//    ui_.gridParams->addWidget(doubleParams_.back()->spinBoxParamValue,     iRow, 1, 1, 1);
-//    ui_.gridParams->addWidget(doubleParams_.back()->pushButtonChangeParam, iRow, 2, 1, 1);
-
  }
 
 
@@ -97,25 +101,18 @@ void ParametersPlugin::pushButtonRefreshAllPressed() {
   parameter_handler_msgs::GetParameterList::Request req;
   parameter_handler_msgs::GetParameterList::Response res;
 
-  doubleParams_.clear();
 
+  if (getParameterListClient_.call(req,res)) {
+    parameters_ = res.parameters;
 
-//    ui_.comboBoxAvailableParameters->clear();
-
-    if (getParameterListClient_.call(req,res)) {
-      // sort alphabetically
-      std::sort(res.parameters.begin(), res.parameters.end(), compareNoCase );
-      for (auto& name : res.parameters) {
-        doubleParams_.push_back(std::shared_ptr<DoubleParameter>(new DoubleParameter(name, widget_, ui_.gridParams, &getParameterClient_, &setParameterClient_)));
-      }
-    }
-    else {
-      ROS_WARN("Could not get parameter list!");
-    }
-
+    // sort alphabetically
+    std::sort(parameters_.begin(), parameters_.end(), compareNoCase );
+    emit parametersChanged();
+  }
+  else {
+    ROS_WARN("Could not get parameter list!");
+  }
 }
-
-
 
 void ParametersPlugin::shutdownPlugin() {
   getParameterListClient_.shutdown();
@@ -123,22 +120,61 @@ void ParametersPlugin::shutdownPlugin() {
   setParameterClient_.shutdown();
 }
 
-
-
-
 void ParametersPlugin::saveSettings(qt_gui_cpp::Settings& plugin_settings, qt_gui_cpp::Settings& instance_settings) const {
   plugin_settings.setValue("lineEditFilter", ui_.lineEditFilter->displayText());
-//  plugin_settings.setValue("lineEditParamName", ui_.lineEditParamName->displayText());
-//  plugin_settings.setValue("spinBoxParamValue", ui_.spinBoxParamValue->value());
-
 }
 
 
 void ParametersPlugin::restoreSettings(const qt_gui_cpp::Settings& plugin_settings, const qt_gui_cpp::Settings& instance_settings) {
-
   ui_.lineEditFilter->setText(plugin_settings.value("lineEditFilter").toString());
-//  ui_.lineEditParamName->setText(plugin_settings.value("lineEditParamName").toString());
-//  ui_.spinBoxParamValue->setValue(plugin_settings.value("spinBoxParamValue").toDouble());
+}
+
+void ParametersPlugin::drawParamList() {
+
+  doubleParams_.clear();
+
+  if (wScroll_) {
+    // delete widget
+    delete wScroll_->layout();
+    delete client_->layout();
+    ui_.gridLayout->removeWidget(wScroll_);
+    delete wScroll_;
+  }
+
+  wScroll_ = new QWidget();
+  wScroll_->setObjectName(QString::fromUtf8("wScroll"));
+
+  ui_.gridLayout->addWidget(wScroll_, 10, 1, 1, 1);
+
+
+  client_ = new QWidget(wScroll_);
+  newGridParams_= new QGridLayout(client_);
+  newGridParams_->setSpacing(6);
+  newGridParams_->setObjectName(QString::fromUtf8("newGridParams"));
+
+  size_t maxParamNameSize = max_line_length(parameters_);
+
+  std::string filter = ui_.lineEditFilter->text().toStdString();
+  for (auto& name : parameters_) {
+    if (name.compare(0, filter.length(), filter) == 0) {
+      doubleParams_.push_back(std::shared_ptr<DoubleParameter>(new DoubleParameter(name, widget_, newGridParams_, &getParameterClient_, &setParameterClient_, maxParamNameSize)));
+    }
+  }
+  // This needs to be done after everthing is setup.
+  client_->setLayout(newGridParams_);
+
+  // Put it into a scroll area
+  QScrollArea* area = new QScrollArea();
+  area->setWidget(client_);
+
+  // Make the scroll step the same width as the fixed widgets in the grid
+  area->horizontalScrollBar()->setSingleStep(client_->width() / 24);
+
+  layout_ = new QVBoxLayout(wScroll_);
+  layout_->addWidget(area);
+
+  wScroll_->setLayout(layout_);
+  client_->setLayout(newGridParams_);
 
 }
 
